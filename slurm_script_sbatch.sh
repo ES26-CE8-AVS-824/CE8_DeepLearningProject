8da3487b3934d4edf6a0b1974d38485b8eca44b2
selendic <markselendic@gmail.com>
2026-04-10 12:07:38 +0200

    Slurm script modified to use multiple GPUs.

--- slurm_script_sbatch.sh
+++ slurm_script_sbatch.sh
@@ -4,13 +4,13 @@
 #SBATCH --output=logs/mini-whisper_%j.out
 #SBATCH --error=logs/mini-whisper_%j.err
 #SBATCH --mem=24G
-#SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:4
 #SBATCH --time=12:00:00
 
-singularity exec --nv \
+singularity exec --nv --cleanenv \
     -B "$HOME/.singularity:/scratch/singularity" \
-    --env WANDB_API_KEY="[wandb-api-key]" \
+    --env WANDB_API_KEY="[paste_here_your_wandb_api_key]" \
     --env WANDB_ENTITY="mini-whisper" \
     --env WANDB_PROJECT="mini-whisper" \
     "/ceph/project/es26-ce8-avs-824/whispers-in-the-storm/sgmse_env_cu130_v1.sif" \
@@ -19,5 +19,5 @@ singularity exec --nv \
         export TMPDIR=/scratch/singularity/tmp && \
         export TRITON_LIBCUDA_PATH=/.singularity.d/libs && \
         export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True && \
-        python train.py
+        torchrun --nproc_per_node=4 train.py
     "
